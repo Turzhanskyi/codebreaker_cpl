321abf3f506119e571876e7fe68328d9968576fb
Turzhanskyi <turzhansky81@gmail.com>
2020-07-02 10:00:52 +0300

    Create gem with bundler

---
+++ lib/cpl_codebreaker.rb
@@ -0,0 +1,6 @@
+require "cpl_codebreaker/version"
+
+module CplCodebreaker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cpl_codebreaker/version.rb
@@ -0,0 +1,3 @@
+module CplCodebreaker
+  VERSION = "0.1.0"
+end
